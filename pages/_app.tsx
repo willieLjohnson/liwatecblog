9d51cbd08d97279ddbd3cba56ff403a65d6d3502
Willie Johnson <liwa.johnson@gmail.com>
2023-01-04 18:26:13 -0500

    remove the old google analytic methods

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,12 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
-import { Analytics } from "@vercel/analytics/react";
-
 import { config, library } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
-import { GAID } from "../lib/constants";
-import Script from "next/script";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 library.add(fab, faCog);
-import * as gtag from "../lib/gtag";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return <Component {...pageProps} />;
 }
