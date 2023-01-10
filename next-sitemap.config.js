bc122623ac804b00c86624c203eae57a9b5c88bb
Willie Johnson <liwa.johnson@gmail.com>
2023-01-10 18:40:04 -0500

    fix typo in site-url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.wllieliwa.com',
+  siteUrl: process.env.SITE_URL || 'https://www.willieliwa.com',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
